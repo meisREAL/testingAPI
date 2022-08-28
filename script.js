7be60fad4f4663f64180da55a6c3162cc50d7e74
meisReal <simas.cesnakauskas@gmail.com>
2022-08-28 09:41:42 +0300

    function rewriten with async/await

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+// const img = document.querySelector('img');
+
+// fetch('https://api.giphy.com/v1/gifs/translate?api_key=MY-API-KEY=cats', { mode: 'cors' })
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (response) {
+//         img.src = response.data.images.original.url;
+//     })
+
 const img = document.querySelector('img');
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=MY-API-KEY=cats', { mode: 'cors' })
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (response) {
-        img.src = response.data.images.original.url;
-    })
+async function getCats() {
+    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=Oh4cxwMZTwzhwwAmCzkmeyMi1MxDcBJZ&s=cats', { mode: 'cors' })
+    const catData = await response.json();
+    img.src = catData.data.images.original.url;
+}
+
+getCats();
